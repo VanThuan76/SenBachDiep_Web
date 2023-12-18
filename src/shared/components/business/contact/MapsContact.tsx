80ae624107112521527db18ba791b67272ea8d68
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-12-19 00:21:55 +0700

    thuanvv/fix-contact-page

--- src/shared/components/business/contact/MapsContact.tsx
+++ src/shared/components/business/contact/MapsContact.tsx
@@ -1,43 +1,27 @@
 import useTrans from '@/src/shared/hooks/useTrans';
-import { Loader } from '@googlemaps/js-api-loader';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const MapsContact = () => {
   const { trans } = useTrans();
-  const mapRef = React.useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const initMap = async () => {
-      const loader = new Loader({
-        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEYS as string,
-        version: 'weekly',
-      });
-      const { Map } = await loader.importLibrary('maps');
-      const { Marker } = (await loader.importLibrary('marker')) as google.maps.MarkerLibrary;
-      const position = {
-        lat: 20.9972262,
-        lng: 105.8650064,
-      };
-      const mapOptions: google.maps.MapOptions = {
-        center: position,
-        zoom: 17,
-        mapId: 'SEN_BACH_DIEP',
-      };
-      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
-      const marker = new Marker({
-        map: map,
-        position: position,
-      });
-    };
-    initMap();
-  }, []);
-  if (mapRef === null) return <></>;
   return (
     <section id='MapsContact' className='w-screen px-4 pb-4 md:px-24 md:pb-8 lg:pb-10 xl:pb-24'>
       <h1 className='mt-5 font-bold text-2xl mb-3 border-b border-b-[#555]'>{trans.common.home}</h1>
-      <p className='font-bold text-md mb-3'>{trans.common.phone_number}: {trans.common.business_info.phone_number}</p>
+      <p className='font-bold text-md mb-3'>
+        {trans.common.phone_number}: {trans.common.business_info.phone_number}
+      </p>
       <p className='font-bold text-md mb-3'>Email: {trans.common.business_info.email}</p>
-      <p className='font-bold text-md mb-3'>{trans.common.address}: {trans.common.business_info.address}</p>
-      <div className='min-h-[550px]' ref={mapRef}></div>
+      <p className='font-bold text-md mb-3'>
+        {trans.common.address}: {trans.common.business_info.address}
+      </p>
+      <iframe
+        src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14899.54793978821!2d105.8680589!3d20.9971669!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135add09c5daa7b%3A0xb977de42fa0f8ce3!2sCentury%20Tower!5e0!3m2!1svi!2s!4v1702919974958!5m2!1svi!2s'
+        width='1280'
+        height='450'
+        style={{ border: 0 }}
+        allowFullScreen={true}
+        loading='lazy'
+        referrerPolicy='no-referrer-when-downgrade'
+      ></iframe>{' '}
     </section>
   );
 };
